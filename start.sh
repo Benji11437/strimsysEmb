ed92e9ac33a6077ab475a06592aa1221c781eb97
Benji11437 <159194518+Benji11437@users.noreply.github.com>
2025-12-05 05:39:44 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🚀 Lancement de Flask (Gunicorn)..."
-gunicorn -b 0.0.0.0:5000 serveur:app --timeout 600 --workers 1 --threads 2 &
-
-echo "🚀 Lancement de Streamlit..."
-streamlit run main.py --server.port 8501 --server.address 0.0.0.0
